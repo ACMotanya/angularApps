bfd4c817e3cd5b360ed2f4906c1da3ea6515bc6e
Azubuike Motanya <acmotanya@gmail.com>
2015-08-29 23:12:01 -0400

    adding some Northwind models and controllers

--- northwindApp/app/tests/category.server.model.test.js
+++ northwindApp/app/tests/category.server.model.test.js
@@ -5,51 +5,75 @@
  */
 var should = require('should'),
 	mongoose = require('mongoose'),
-	User = mongoose.model('User'),
 	Category = mongoose.model('Category');
-
-/**
- * Globals
- */
-var user, category;
 
 /**
  * Unit tests
  */
-describe('Category Model Unit Tests:', function() {
-	beforeEach(function(done) {
-		user = new User({
-			firstName: 'Full',
-			lastName: 'Name',
-			displayName: 'Full Name',
-			email: 'test@test.com',
-			username: 'username',
-			password: 'password'
-		});
+describe('Category Model', function() {
 
-		user.save(function() { 
-			category = new Category({
-				// Add model fields
-				// ...
+	describe('Saving', function() {
+		it('saves new record', function(done) {
+			var category = new Category({
+				name: 'Beverages',
+				description: 'Soft Drinks, coffees, teas, beers, and ales'
 			});
 
-			done();
-		});
-	});
-
-	describe('Method Save', function() {
-		it('should be able to save without problems', function(done) {
-			return category.save(function(err) {
+			category.save(function(err, saved) {
 				should.not.exist(err);
 				done();
 			});
 		});
+
+		it('throws validation error when name is empty', function(done) {
+			var category = new Category({
+				description: 'Soft drinks, coffees, teas, beers, and ales'
+			});
+
+			category.save(function(err) {
+				should.exist(err);
+				err.errors.name.message.should.equal('name cannot be blank');
+				done();
+			});
+		});
+
+		it('throws validaton error when name longer than 15 chars', function(done) {
+			var category = new Category({
+				name: 'Grains/Cereals/Chocolates'
+			});
+
+			category.save(function(err) {
+				should.exist(err);
+				err.errors.name.message.should.equal('name must be 15 chars in length or less');
+				done();
+			});
+		});
+
+		it('throws validaton error for duplicte category name', function(done) {
+			var category = new Category({
+				name: 'Beverages'
+			});
+
+			category.save(function(err) {
+				should.not.exist(err);
+
+				var duplicte = new Category({
+					name: 'Beverages'
+				});
+
+				duplicte.save(function(err) {
+					err.err.indexOf('$name').should.not.equal(-1);
+					err.err.indexOf('duplicate key error').should.not.equal(-1);
+					should.exist(err);
+					done();
+				});
+			});
+		});
 	});
 
-	afterEach(function(done) { 
+	afterEach(function(done) {
+		// NB this deletes ALL categories (but is run against a test database)
 		Category.remove().exec();
-		User.remove().exec();
-
 		done();
 	});
 });
